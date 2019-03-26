dd480ebd7aa7867260bbee346da5d9e002621b0d
Lidii <lidiiany92@gmail.com>
2019-03-26 12:26:00 -0400

    Added bodyParser middleware

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config({path: 'variables.env'});
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
+
+//Bring in GraphQL/Express Middleware
+const { graphiqlExpress, graphqlExpress} = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const {typeDefs} = require('./schema');
+const {resolvers} = require('./resolvers');
+
+//Create GraphQL schema
+const schema = makeExecutableSchema({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+});
 
 //Connect to database
 mongoose
@@ -13,6 +27,17 @@ mongoose
 //Initializes application
 const app = express();
 
+//create GraphiQL application
+app.use('/graphiql', graphiqlExpress({endpointURL: 'graphql'}));
+
+//Connect schemas with GraphQL
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema: schema,
+    context: {
+        Recipe,
+        User
+    }
+}));
 
 const PORT = process.env.PORT || 4444;
 
