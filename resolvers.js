f243380abd63cac8b938ef0c3b473e5b1a02887b
Lidii <lidiiany92@gmail.com>
2019-03-27 13:48:29 -0400

    Created Query for getting all recipes and mutation for adding recipe

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,23 @@
 exports.resolvers = {
     Query: {
-        getAllRecipes: () => {}
+        getAllRecipes: async (root, args, {Recipe}) => {
+            const allRecipes = await Recipe.find();
+            return allRecipes;
+        }
+    },
+
+    Mutation: {
+        addRecipe: async (root, {name, category, description, instructions, username}, {Recipe}) => {
+            const newRecipe = await new Recipe({
+                name,
+                category,
+                description,
+                instructions,
+                username
+            }).save();
+
+            return newRecipe;
+        }
     }
+
 };
