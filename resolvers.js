151116cf8c304ca8f7c923c40ac20cd06fc45ada
Lidii <lidiiany92@gmail.com>
2019-03-31 15:12:14 -0400

    Created signup user mutation

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,10 @@
+const jwt = require('jsonwebtoken');
+
+const createToken = (user, secret, expiresIn) => {
+    const {username, email} = user; 
+    return jwt.sign({username, email}, secret, {expiresIn})
+}
+
 exports.resolvers = {
     Query: {
         getAllRecipes: async (root, args, {Recipe}) => {
@@ -17,6 +24,21 @@ exports.resolvers = {
             }).save();
 
             return newRecipe;
+        },
+
+        signupUser: async(root, {username, email, password}, {User}) => {
+            const userForCheck = await User.findOne({username: username});
+            if(userForCheck) {
+                throw new Error('User already exists');
+            }
+
+            const newUser = await new User({
+                username,
+                email,
+                password
+            }).save();
+
+            return {token: createToken(newUser, process.env.SECRET, '1hr')};
         }
     }
 
--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+
 exports.typeDefs = `
 
     type Recipe {
@@ -24,7 +25,13 @@ exports.typeDefs = `
         getAllRecipes: [Recipe]
     }
 
+    type Token {
+        token: String! 
+    }
+
     type Mutation {
         addRecipe(name: String!, category: String!, description: String!, instructions: String!, username: String): Recipe
+
+        signupUser(username: String!, email: String!, password: String!): Token
     }
 `;
