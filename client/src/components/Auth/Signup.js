35d2b563a2c1beb7596983a973709bc0f69ab9f5
Lidii <lidiiany92@gmail.com>
2019-04-01 16:07:28 -0400

    Added handleChange functionality for the signup component

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
 class Signup extends React.Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+    }
+
+
+
     render() {
+        const {username, email, password, passwordConfirmation} = this.state;
+
         return (
             <div className="App">
                 <h2 className="App">Signup</h2>
                 <form className="form">
-                    <input type="text" name="username" placeholder="Username"/>
-                    <input type="email" name="email" placeholder="Email Address"/>
-                    <input type="password" name="password" placeholder="Password"/>
-                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password"/>
+                    <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange}/>
+                    <input type="email" name="email" placeholder="Email Address" value={email} onChange={this.handleChange}/>
+                    <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange}/>
+                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password" value={passwordConfirmation} onChange={this.handleChange}/>
                     <button type="submit" className="button-primary">Submit</button>
                 </form>
             </div>
