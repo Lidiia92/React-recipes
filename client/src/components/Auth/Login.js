d50ca2887557ba013f490a46b25301479ef60be8
Lidii <lidiiany92@gmail.com>
2019-04-02 12:27:56 -0400

    Implemented login mutation on the client side

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import {Mutation} from 'react-apollo';
+import {SIGNIN_USER} from '../../queries/index';
+
+import Error from '../Error';
+
+const initialState = {
+    username: "",
+    password: ""
+}
 
 class Login extends React.Component {
+    state = {
+        ...initialState
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    handleSubmit = async (e, signinUser) => {
+        e.preventDefault();
+        const data = await signinUser();
+        this.clearState();
+        console.log('data Login.js', data);
+    }
+
+    clearState = () => {
+        this.setState({
+            ...initialState
+        })
+    }
+
+    validateForm = () => {
+        const {username, password } = this.state;
+        if (!username || !password ) return true;
+    }
+
+
     render() {
+        const {username, password} = this.state;
+
         return (
-            <div>Login</div>
+            <div className="App">
+                <h2 className="App">Signin</h2>
+                <Mutation mutation={SIGNIN_USER} variables={{username, password}}>
+                    {(signinUser, {data, loading, error}) => {
+
+                        return (
+                            <form className="form" onSubmit={(event) => this.handleSubmit(event, signinUser)}>
+                                <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange}/>
+                                <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange}/>
+                                <button type="submit" className="button-primary" disabled={loading || this.validateForm()}>Submit</button>
+
+                                {error && <Error error={error}/>}
+                            </form>
+                        );
+                    }}
+                </Mutation>
+            </div>
         );
     }
 }
