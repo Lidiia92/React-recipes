c7d5342d38f6dc55d70f63c7cd1c18b860601555
Lidii <lidiiany92@gmail.com>
2019-04-03 12:14:17 -0400

    Added getCurrentUser query

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({path: 'variables.env'});
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
@@ -37,21 +38,34 @@ app.use(cors(corsOptions));
 //Set up jwt authentication middleware
 app.use( async (req, res, next) => {
     const token = req.headers['authorization'];
-    console.log(token);
+    if(token !== "null") {
+        try{
+            const currentUser = await jwt.verify(token, process.env.SECRET);
+            req.currentUser = currentUser;
+        } catch(err) {
+            console.error(err);
+        }
+    }
     next();
+
 });
 
 //create GraphiQL application
 app.use('/graphiql', graphiqlExpress({endpointURL: 'graphql'}));
 
 //Connect schemas with GraphQL
-app.use('/graphql', bodyParser.json(), graphqlExpress({
+app.use('/graphql', bodyParser.json(), 
+
+graphqlExpress(({currentUser}) => ({
     schema: schema,
     context: {
         Recipe,
-        User
+        User,
+        currentUser
     }
-}));
+}))
+
+);
 
 const PORT = process.env.PORT || 4444;
 
