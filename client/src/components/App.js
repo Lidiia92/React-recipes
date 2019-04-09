c2c240f6fe8543a234a642725d30086d7bfdef3d
Lidii <lidiiany92@gmail.com>
2019-04-09 10:21:09 -0400

    Displaying recipe item component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { Query } from 'react-apollo';
 import { GET_ALL_RECIPES } from '../queries/index';
 
+import RecipeItem from './Recipe/RecipeItem';
+
 const App = () => (
   <div className="App">
     <h1>Home</h1>
@@ -12,7 +14,9 @@ const App = () => (
 			if(loading) return <div>Loading</div>
 			if(error) return <div>Error</div>
 			console.log('data', data, loading, error);
-			return <p>Recipes</p>
+			return <ul>{data.getAllRecipes.map(recipe => 
+        <RecipeItem key={recipe._id} name={recipe.name} category={recipe.category}/>
+        )}</ul>
       }}
     </Query>
   </div>
